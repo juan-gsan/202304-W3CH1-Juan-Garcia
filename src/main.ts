c9771d5fe4dc3443a5836adab93029acf1b9f532
JuanG <juan.gsanchez13@gmail.com>
2023-05-15 23:30:53 +0200

    Update typescript files

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createCharacters } from './createCharacters.ts';
 const charactersContainer = createCharacters();
 const template = document.querySelector('.characters-list')!;
 
-const cardJoffrey = `<li class="character col">
+let cardJoffrey = `<li class="character col">
         <div class="card character__card">
           <img src="img/joffrey.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
           <div class="card-body">
@@ -14,7 +14,6 @@ const cardJoffrey = `<li class="character col">
                 <li>Edad: ${charactersContainer[0].age}</li>
                 <li>
                   Estado:
-                  <i class="fas fa-thumbs-down"></i>
                   <i class="fas fa-thumbs-up"></i>
                 </li>
               </ul>
@@ -29,11 +28,160 @@ const cardJoffrey = `<li class="character col">
               </div>
             </div>
           </div>
-          <i class="emoji"></i>
+          <i class="👑"></i>
         </div>
       </li>`;
 
-const cardJaime = `<li class="character col">
+let cardJaime = `<li class="character col">
+        <div class="card character__card">
+          <img src="img/jaime.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${charactersContainer[1].name} ${charactersContainer[1].house}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${charactersContainer[1].age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Arma que usa: ${charactersContainer[1].weapon}</li>
+                <li>Destreza: ${charactersContainer[1].skills}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class=" 🗡"></i>
+        </div>
+      </li>`;
+
+let cardDaenerys = `<li class="character col">
+        <div class="card character__card">
+          <img src="img/daenerys.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${charactersContainer[2].name} ${charactersContainer[2].house}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${charactersContainer[2].age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Arma que usa: ${charactersContainer[2].weapon}</li>
+                <li>Destreza: ${charactersContainer[2].skills}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class=" 🗡"></i>
+        </div>
+      </li>`;
+
+let cardTyrion = `<li class="character col">
+        <div class="card character__card">
+          <img src="img/tyrion.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${charactersContainer[3].name} ${charactersContainer[3].house}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${charactersContainer[3].age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Personaje al que asesora: ${charactersContainer[3].characterAdvised}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="🎓"></i>
+        </div>
+      </li>`;
+
+let cardBronn = `<li class="character col">
+        <div class="card character__card">
+          <img src="img/bronn.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${charactersContainer[4].name} ${charactersContainer[4].house}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${charactersContainer[4].age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Personaje al que sirve: ${charactersContainer[4].knight}</li>
+                <li>Pelotismo: ${charactersContainer[4].pelotismo}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class=" 🛡"></i>
+        </div>
+      </li>`;
+
+template.innerHTML =
+  cardJoffrey + cardJaime + cardDaenerys + cardTyrion + cardBronn;
+
+if (!charactersContainer[0].alive) {
+  cardJoffrey = `<li class="character col">
+        <div class="card character__card">
+          <img src="img/joffrey.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${charactersContainer[0].name} ${charactersContainer[0].house}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${charactersContainer[0].age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-down"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Años de reinado: ${charactersContainer[0].rulingYears}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="👑"></i>
+        </div>
+      </li>`;
+}
+
+if (!charactersContainer[1].alive) {
+  cardJaime = `<li class="character col">
         <div class="card character__card">
           <img src="img/jaime.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
           <div class="card-body">
@@ -44,13 +192,13 @@ const cardJaime = `<li class="character col">
                 <li>
                   Estado:
                   <i class="fas fa-thumbs-down"></i>
-                  <i class="fas fa-thumbs-up"></i>
                 </li>
               </ul>
             </div>
             <div class="character__overlay">
               <ul class="list-unstyled">
-                
+                <li>Arma que usa: ${charactersContainer[1].weapon}</li>
+                <li>Destreza: ${charactersContainer[1].skills}</li>
               </ul>
               <div class="character__actions">
                 <button class="character__action btn">habla</button>
@@ -58,11 +206,13 @@ const cardJaime = `<li class="character col">
               </div>
             </div>
           </div>
-          <i class="emoji"></i>
+          <i class=" 🗡"></i>
         </div>
       </li>`;
+}
 
-const cardDaenerys = `<li class="character col">
+if (!charactersContainer[2].alive) {
+  cardDaenerys = `<li class="character col">
         <div class="card character__card">
           <img src="img/daenerys.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
           <div class="card-body">
@@ -73,13 +223,13 @@ const cardDaenerys = `<li class="character col">
                 <li>
                   Estado:
                   <i class="fas fa-thumbs-down"></i>
-                  <i class="fas fa-thumbs-up"></i>
                 </li>
               </ul>
             </div>
             <div class="character__overlay">
               <ul class="list-unstyled">
-                
+                <li>Arma que usa: ${charactersContainer[2].weapon}</li>
+                <li>Destreza: ${charactersContainer[2].skills}</li>
               </ul>
               <div class="character__actions">
                 <button class="character__action btn">habla</button>
@@ -87,11 +237,13 @@ const cardDaenerys = `<li class="character col">
               </div>
             </div>
           </div>
-          <i class="emoji"></i>
+          <i class=" 🗡"></i>
         </div>
       </li>`;
+}
 
-const cardTyrion = `<li class="character col">
+if (!charactersContainer[3].alive) {
+  cardTyrion = `<li class="character col">
         <div class="card character__card">
           <img src="img/tyrion.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
           <div class="card-body">
@@ -102,13 +254,12 @@ const cardTyrion = `<li class="character col">
                 <li>
                   Estado:
                   <i class="fas fa-thumbs-down"></i>
-                  <i class="fas fa-thumbs-up"></i>
                 </li>
               </ul>
             </div>
             <div class="character__overlay">
               <ul class="list-unstyled">
-                
+                <li>Personaje al que asesora: ${charactersContainer[3].characterAdvised}</li>
               </ul>
               <div class="character__actions">
                 <button class="character__action btn">habla</button>
@@ -116,11 +267,13 @@ const cardTyrion = `<li class="character col">
               </div>
             </div>
           </div>
-          <i class="emoji"></i>
+          <i class="🎓"></i>
         </div>
       </li>`;
+}
 
-const cardBronn = `<li class="character col">
+if (!charactersContainer[4].alive) {
+  cardBronn = `<li class="character col">
         <div class="card character__card">
           <img src="img/bronn.jpg" alt="Nombre y familia del personaje" class="character__picture card-img-top" />
           <div class="card-body">
@@ -131,13 +284,13 @@ const cardBronn = `<li class="character col">
                 <li>
                   Estado:
                   <i class="fas fa-thumbs-down"></i>
-                  <i class="fas fa-thumbs-up"></i>
                 </li>
               </ul>
             </div>
             <div class="character__overlay">
               <ul class="list-unstyled">
-                
+                <li>Personaje al que sirve: ${charactersContainer[4].knight}</li>
+                <li>Pelotismo: ${charactersContainer[4].pelotismo}</li>
               </ul>
               <div class="character__actions">
                 <button class="character__action btn">habla</button>
@@ -145,9 +298,19 @@ const cardBronn = `<li class="character col">
               </div>
             </div>
           </div>
-          <i class="emoji"></i>
+          <i class=" 🛡"></i>
         </div>
       </li>`;
+}
 
-template.innerHTML =
-  cardJoffrey + cardJaime + cardDaenerys + cardTyrion + cardBronn;
+const speakButton = document.querySelector('.character__action')!;
+
+speakButton.addEventListener('click', () => {
+  const appContainer = document.querySelector('.app')!;
+  appContainer.innerHTML =
+    template +
+    `<div class="comunications">
+    <p class="comunications__text display-1">${charactersContainer[0].communicate}</p>
+    <img class="comunications__picture" src="img/joffrey.jpg" alt="Joffrey Baratheon" />
+  </div>`;
+});
